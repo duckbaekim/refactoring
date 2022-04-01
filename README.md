ceb18efd600bd24a60cbea16fbfac4658e10c0d4
inqu <inqu16@gmail.com>
2022-04-01 19:34:29 +0900

    리펙토링 전

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # refactoring
+
 refactoring
+
+## refactoring 2 필기하며 따라하기
---
+++ main.js
@@ -0,0 +1,49 @@
+function statement(invoice, plays) {
+  let total_amount = 0;
+  let volume_credits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let this_amount = 0;
+
+    switch (play.type) {
+      case "tragedy": //비극
+        this_amount = 40000;
+        if (perf.audience > 30) {
+          this_amount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy": //희극
+        this_amount = 30000;
+        if (perf.audience > 20) {
+          this_amount = 10000 + 500 * (perf.audience - 20);
+        }
+        this_amount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    // 포인트를 적립한다.
+    volume_credits += Math.max(perf.audience - 30, 0);
+
+    //희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) volume_credits += Math.floor(perf.audience / 5);
+
+    //청구 내역을 출력한다.
+    result += ` ${play.name}: ${format(this_amount / 100)} (${
+      perf.audience
+    }석)\n`;
+    total_amount += this_amount;
+  }
+  result += `총액: ${format(total_amount / 100)}\n`;
+  result += `적립 포인트 : ${volume_credits}점\n`;
+  return result;
+}
